bd2f5be9ff2ab510f8b65b04a01f06d952c70dcb
Frederico Machado da Silva <fredmachado@gmail.com>
2016-05-12 15:30:06 -0300

    versão inicial

---
+++ server.js
@@ -0,0 +1,4 @@
+var http = require ('http');
+var app = require('./config/express')();
+
+http.createServer(app).listen(app.get('port'),console.log('Express Server escutando na porta ' + app.get('port')));
